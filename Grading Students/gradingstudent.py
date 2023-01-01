26b11be0b78befdc4759b5507cb4d7634bb22f85
Dagim Fikru <95189710+Dagim-Fikru@users.noreply.github.com>
2023-01-01 19:56:20 -0100

    Create gradingstudent.py
    problem-2

---
+++ Grading Students/gradingstudent.py
@@ -0,0 +1,13 @@
+def gradingStudents(grades):
+    # Write your code here
+    newGrades=[]
+    for grade in grades:
+        if grade <38:
+            newGrades.append(grade)
+        elif (grade+1)%5==0:
+            newGrades.append(grade+1)
+        elif (grade+2)%5==0:
+            newGrades.append(grade+2)
+        else:
+            newGrades.append(grade)
+    return newGrade
