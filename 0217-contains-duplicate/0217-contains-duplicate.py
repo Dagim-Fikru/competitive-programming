8afdba98da9cc882e76470cf4a6ee0cfb9282a5a
Dagim Fikru <95189710+Dagim-Fikru@users.noreply.github.com>
2023-03-22 15:29:18 +0300

    Time: 577 ms (50.95%), Space: 31.6 MB (14.76%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,22 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        freq = collections.Counter(nums)
+        key = freq.values()
+        isYes = False
+        for key in freq:
+            if freq[key]>1:
+                isYes=True
+                break
+        # setOfList = set(nums)
+        # for i in setOfList:
+        #     if nums.count(i)>1:
+        #        isYes = True
+        #        break
+            # else:
+            #     return False
+        # for i in nums:
+        #     if nums.count(i)>1:
+        #         return True
+        #     else:
+        #         return False
+        return isYes
