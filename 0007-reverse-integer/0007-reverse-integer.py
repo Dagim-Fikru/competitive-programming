4e479e8fdcd7aab942ed6354eecb988123931448
Dagim Fikru <95189710+Dagim-Fikru@users.noreply.github.com>
2023-03-23 15:27:27 +0300

    Time: 37 ms (44.48%), Space: 13.9 MB (9.44%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,21 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        y=str(x)
+        y=y[::-1]
+        arr=[]
+        for i in y:
+            arr.append(i)
+        if arr[-1]=='-':
+            arr.pop()
+            arr.insert(0,'-')
+        y=int(''.join(arr))
+        if y<=-2**31 or y>=(2**31)-1:
+            return 0
+        else:
+            return y
+        # arr=y.split(' ')
+        # if y[-1]=='-':
+        # y[-1]=y[0]
+        # print(int(y))
+        
+        
