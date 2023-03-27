e53854005f57443b162355e715a9eb4c7fab8e10
Dagim Fikru <95189710+Dagim-Fikru@users.noreply.github.com>
2023-03-27 12:45:42 +0300

    Time: 4791 ms (10.12%), Space: 24.5 MB (5.25%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -0,0 +1,18 @@
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        def fac(x):
+            if x==1 or x==0:
+                return 1
+            else:
+                return x * fac(x-1)
+        nFac = str(fac(n))
+        arr=[]
+        count=0
+        for i in nFac:
+            arr.append(i)
+        while arr[-1]=='0':
+            count+=1
+            arr.pop()
+        return count
+        
+        
