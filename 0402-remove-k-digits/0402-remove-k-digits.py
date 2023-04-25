a2dbca6aece7335b87fd6e97edbeb856a4f4b1eb
Dagim Fikru <95189710+Dagim-Fikru@users.noreply.github.com>
2023-04-25 23:58:36 +0300

    Time: 73 ms (62.85%), Space: 15.5 MB (31.20%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.py
+++ 0402-remove-k-digits/0402-remove-k-digits.py
@@ -10,29 +10,7 @@ class Solution:
         if k > 0:
             stack = stack[:-k]     
         return "".join(stack).lstrip("0") or "0"
-#         count=0
-#         i = 0
-#         if len(num)==1:
-#             return "0"
-#         elif len(set(num))==1:
-#             while k>0:
-#                 arr.pop()
-#                 k-=1
-#         while i<=len(arr)-2:
-#             if arr[i]>arr[i+1]:
-#                 arr.pop(i)
-#                 count+=1
-#             elif arr[i]<arr[i+1]:
-#                 arr.pop(i+1)
-#                 count+=1
-#             else:
-#                 i+=1
-#             if k==count:
-#                 break
-            
-#         my_string = ''.join(str(i) for i in arr)
-#         my_string=int(my_string)
-#         return str(my_string)
+
             
             
         
