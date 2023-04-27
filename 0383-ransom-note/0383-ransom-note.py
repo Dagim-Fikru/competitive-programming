9d7dd868b084dbfa169fe904d19c2a9eb536fce6
Dagim Fikru <95189710+Dagim-Fikru@users.noreply.github.com>
2023-04-27 21:52:23 +0300

    Time: 190 ms (5.25%), Space: 16.7 MB (9.62%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,17 +1,17 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        arr1= list(ransomNote)
-        arr2=list(magazine)
-        for i in arr1:
-            if i in arr2 and arr1.count(i)<=arr2.count(i):
+        # arr1= list(ransomNote)
+        # # arr2=list(magazine)
+        # # for i in arr1:
+        # #     if i in arr2 and arr1.count(i)<=arr2.count(i):
+        # #         continue
+        # #     else:
+        # #         return False
+        # # return True
+        
+        for i in ransomNote:
+            if i in magazine and ransomNote.count(i)<=magazine.count(i) :
                 continue
             else:
                 return False
         return True
-        
-        # for i in ransomNote:
-        #     if i in magazine:
-        #         continue
-        #     else:
-        #         return False
-        # return True
