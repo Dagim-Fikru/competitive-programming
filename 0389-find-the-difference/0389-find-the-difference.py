bc5bf5e3c25881e61a9be80add32cdfc99477102
Dagim Fikru <95189710+Dagim-Fikru@users.noreply.github.com>
2023-05-12 10:22:13 +0300

    Time: 54 ms (13.87%), Space: 16.4 MB (8.30%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -7,20 +7,4 @@ class Solution:
                 continue
             else:
                 return i
-                
-#         arr1= list(s)
-#         arr2 = list(t)
-#         while len(arr1)<len(arr2):
-#             arr1.append(' ')
-#         # hash_table = {}
-#         # arr1.sort()
-#         # arr2.sort()
-#         # for i in range(len(arr2)):
-#         #     if arr1[i]==arr2[i]
-            
-#         for i in range(len(arr2)):
-#             if arr2[i] in arr1 or arr1[i]==" ":
-#                 continue
-#             else:
-#                 return arr2[i]
             
