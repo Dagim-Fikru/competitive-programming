41e157824b5b4dcaf8227bd04f43b833b1797ed3
Dagim Fikru <95189710+Dagim-Fikru@users.noreply.github.com>
2023-05-17 10:40:20 +0300

    Time: 834 ms (30.49%), Space: 16.3 MB (15.05%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -0,0 +1,15 @@
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        i=1
+        if n==1:
+            return n
+        while n>0:
+            n-=i
+            i+=1
+        if n==0:
+            return i-1
+        else:
+            return i-2
+        
+            
+        
