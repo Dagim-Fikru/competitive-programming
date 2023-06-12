f77bb2ca5ecdacd7f3c58fe93656aa31a245d2a1
Dagim Fikru <95189710+Dagim-Fikru@users.noreply.github.com>
2023-06-12 14:13:33 +0300

    Time: 3575 ms (5.01%), Space: 16.8 MB (51.57%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,20 +1,26 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        x=None
-        listOfS = list(s)
-        listOfT = list(t)
-        listOfS.sort()
-        listOfT.sort()
-        if len(listOfS)!=len(listOfT):
+        if len(s)!=len(t):
             return False
-        else:
-            for i in range(len(listOfS)):
-                if listOfS[i]!=listOfT[i]:
-                    x=False
-                    break
-                else:
-                    x=True
-        return x
+        for i in t:
+            if i not in s or t.count(i)!=s.count(i):
+                return False
+        return True
+        # x=None
+        # listOfS = list(s)
+        # listOfT = list(t)
+        # listOfS.sort()
+        # listOfT.sort()
+        # if len(listOfS)!=len(listOfT):
+        #     return False
+        # else:
+        #     for i in range(len(listOfS)):
+        #         if listOfS[i]!=listOfT[i]:
+        #             x=False
+        #             break
+        #         else:
+        #             x=True
+        # return x
                     
         # for i in s:
         #     if i not in t or len(t)!=len(s):
