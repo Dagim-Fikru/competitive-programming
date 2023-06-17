b054cc7bce543a9a54f49134dd4b757a7006fc92
Dagim Fikru <95189710+Dagim-Fikru@users.noreply.github.com>
2023-06-18 00:39:57 +0300

    Time: 91 ms (82.57%), Space: 17.7 MB (60.52%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -4,8 +4,6 @@ class Solution:
         for i,j in enumerate(nums):
             if target==j:
                 arr.append(i)
-        if len(arr)==1:
-            arr.append(arr[0])
         if len(arr)==0:
             return [-1,-1]
         return [arr[0],arr[-1]]
