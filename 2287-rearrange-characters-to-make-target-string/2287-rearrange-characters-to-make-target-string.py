2e66119529b2f34c4a657e6fb6cbca8292fe93ba
Dagim Fikru <95189710+Dagim-Fikru@users.noreply.github.com>
2023-06-25 23:30:24 +0300

    Time: 43 ms (83.71%), Space: 16.2 MB (94.29%) - LeetHub

--- 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.py
+++ 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.py
@@ -1,21 +1,9 @@
 class Solution:
     def rearrangeCharacters(self, s: str, target: str) -> int:
-        # countS = Counter(s)
-        # countT = Counter(target)
-        # result = 500
-        # for i,j in countT.items():
-        #     result = min(result,countS[i]//j)
-        # return result
         result = 1000
         for i in target:
             result = min(result,(s.count(i)//target.count(i)))
         return result
-            
-        
-        
-        
-        # counter_s = Counter(s)        
-        # return min(counter_s[c] // count for c,count in Counter(target).items())
         
         
         
