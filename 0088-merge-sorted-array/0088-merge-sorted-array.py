23c6cff37a6d20eff09c301fd3ad411eb2ef9c6b
Dagim Fikru <95189710+Dagim-Fikru@users.noreply.github.com>
2023-07-07 00:34:34 +0300

    Time: 56 ms (38.39%), Space: 16.4 MB (55.40%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -8,15 +8,37 @@ class Solution:
         #     m-=1
         # for i in range(n):
         #     nums1.append(nums2[i])
-        arr=[]
-        for i in range(m):
-            arr.append(nums1[i])
-        for j in range(n):
-            arr.append(nums2[j])
-        while nums1:
+        # arr=[]
+        # for i in range(m):
+        #     arr.append(nums1[i])
+        # for j in range(n):
+        #     arr.append(nums2[j])
+        # while nums1:
+        #     nums1.pop()
+        # while arr:
+        #     nums1.append(arr.pop())
+        # nums1.sort()
+        x = len(nums1)-m
+        while x>0:
             nums1.pop()
-        while arr:
-            nums1.append(arr.pop())
-        nums1.sort()
-            
-        
+            x-=1
+        # print(nums1)
+        i=0
+        j=0
+        if len(nums1)==0:
+            for i in nums2:
+                nums1.append(i)
+        else:
+            while j<len(nums2):
+                if i==len(nums1)-1 and nums1[i]<nums2[j]:
+                    nums1.append(nums2[j])
+                    j+=1
+                elif nums1[i]<nums2[j]:
+                    i+=1
+                else:
+                    nums1.insert(i,nums2[j])
+                    j+=1
+        # print(nums1)
+
+
+
