fcdc682fc7a58ac0622ac0925ea280cb4a16ec55
Dagim Fikru <95189710+Dagim-Fikru@users.noreply.github.com>
2023-10-06 23:27:14 +0300

    Update 2089-find-target-indices-after-sorting-array.py

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
@@ -1,11 +1,19 @@
 class Solution:
     def targetIndices(self, nums: List[int], target: int) -> List[int]:
             nums.sort()
+            mappin = {}
             arr=[]
-            if nums.count(target)==1:
-                arr.append(nums.index(target))
-            else:
-                for i in range(nums.count(target)):
-                    arr.append(nums.index(target)+i)
+            for i in range(len(nums)):
+                if nums[i]==target:
+                    mappin[target]=i
+                    arr.append(mappin[target])
             return arr
+            # nums.sort()
+            # arr=[]
+            # if nums.count(target)==1:
+            #     arr.append(nums.index(target))
+            # else:
+            #     for i in range(nums.count(target)):
+            #         arr.append(nums.index(target)+i)
+            # return arr
         
