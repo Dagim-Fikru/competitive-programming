41ab0693503a82fdb2d37eeb5c895cf7d1190f59
Dagim Fikru <95189710+Dagim-Fikru@users.noreply.github.com>
2023-10-11 22:48:41 +0300

    Time: 168 ms (95.47%), Space: 28.6 MB (7.71%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,6 +3,31 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        # arr=[]
+        # while k>=1:
+        #     arr.append(nums.pop())
+        #     k-=1
+        # arr2 = arr[:]
+        # for i in range(len(arr)):
+        #     nums.insert(i,arr2.pop())
+        # while arr:
+        #     nums.append(arr.pop())
+        # if len(nums)<=2:
+        #     pass
+        # else:
+        #     arr=[]
+        #     i=0
+        #     j=len(nums)-k
+        #     while i<len(nums)-k:
+        #         if j<=len(nums)-1:
+        #             arr.append(nums[j])
+        #             j+=1
+        #         else:
+        #             arr.append(nums[i])
+        #             i+=1
+        #     while nums:
+        #         nums.pop()
+        #     nums.extend(arr)
         x = Deque(nums)
         x.rotate(k)
         while nums:
