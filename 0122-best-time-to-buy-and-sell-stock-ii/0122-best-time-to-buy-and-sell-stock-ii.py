38dfb7d6f8bf2eba1bd4fbc384e0e154d412397b
Dagim Fikru <95189710+Dagim-Fikru@users.noreply.github.com>
2023-11-01 22:42:41 +0300

    Time: 54 ms (96.55%), Space: 17.6 MB (69.96%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,16 +1,17 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        arr=[]
+        # arr=[]
+        maxProfit = 0
         i=0
         j=1
         
         while j<len(prices):
             if prices[i]<prices[j]:
-                arr.append(prices[j]-prices[i])
+                maxProfit+=prices[j]-prices[i]
                 i+=1
             else:
                 i=j
             j+=1
         # arr.sort()
-        return sum(arr)
+        return maxProfit
                 
