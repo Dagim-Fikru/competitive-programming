838d3311120c885b71734656a6350a30f8b8233b
Dagim Fikru <95189710+Dagim-Fikru@users.noreply.github.com>
2023-11-02 11:33:56 +0300

    Time: 453 ms (18.66%), Space: 18.9 MB (88.45%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
@@ -1,19 +1,39 @@
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-        prev=0
+        r,l = 0,0
         count=0
-        ls=[]
-        visit=False
-        for i in nums:
-            if i==0:
-                visit=True
-                ls.append(prev+count)
-                prev=count
-                count=0
-            if i==1:
+        maxLen = 0
+        while r<len(nums):
+            if nums[r]==0:
                 count+=1
-        if not visit:
-            count-=1
-        ls.append(count+prev)
-        return max(ls)
+            while count>1:
+                if nums[l]==0:
+                    count-=1
+                l+=1
+                
+            # if count1:
+            #     if nums[l]==0:
+            #         count-=1
+            #     l+=1
+            maxLen = max(maxLen,r-l+1)
+            r+=1
+        print(r,l)
+        return maxLen-1
+            
+        # prev=0
+        # count=0
+        # ls=[]
+        # visit=False
+        # for i in nums:
+        #     if i==0:
+        #         visit=True
+        #         ls.append(prev+count)
+        #         prev=count
+        #         count=0
+        #     if i==1:
+        #         count+=1
+        # if not visit:
+        #     count-=1
+        # ls.append(count+prev)
+        # return max(ls)
         
