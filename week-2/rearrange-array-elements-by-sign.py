014750104402550a529d7c0fa86f34cfc84c35a1
Dagim Fikru <95189710+Dagim-Fikru@users.noreply.github.com>
2023-12-08 14:58:43 +0300

    Added solution for Rearrange Array Elements by Sign

--- week-2/rearrange-array-elements-by-sign.py
+++ week-2/rearrange-array-elements-by-sign.py
@@ -1,27 +1,36 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
         arr=[]
-        pos = 0
-        neg = 0
-        while pos<len(nums) or neg<len(nums):
-            if pos<=len(nums)-1:
-                if nums[pos]>0:
-                    if len(arr)==0:
-                        arr.append(nums[pos])
-                        pos+=1
-                    elif arr[-1]<0:
-                        arr.append(nums[pos])
-                        pos+=1
-                elif nums[pos]<0:
-                    pos+=1
-            if neg<=len(nums)-1:
-                if nums[neg]<0:
-                    if len(arr)==0:
-                        pass
-                    else:
-                        if arr[-1]>0:
-                            arr.append(nums[neg])
-                            neg+=1
-                elif nums[neg]>0:
-                    neg+=1
+        even = 0
+        odd = 1
+        for num in nums:
+            if num>0:
+                arr.insert(even,num)
+                even+=2
+            else:
+                arr.insert(odd,num)
+                odd+=2
+        # pos = 0
+        # neg = 0
+        # while pos<len(nums) or neg<len(nums):
+        #     if pos<=len(nums)-1:
+        #         if nums[pos]>0:
+        #             if len(arr)==0:
+        #                 arr.append(nums[pos])
+        #                 pos+=1
+        #             elif arr[-1]<0:
+        #                 arr.append(nums[pos])
+        #                 pos+=1
+        #         elif nums[pos]<0:
+        #             pos+=1
+        #     if neg<=len(nums)-1:
+        #         if nums[neg]<0:
+        #             if len(arr)==0:
+        #                 pass
+        #             else:
+        #                 if arr[-1]>0:
+        #                     arr.append(nums[neg])
+        #                     neg+=1
+        #         elif nums[neg]>0:
+        #             neg+=1
         return arr
