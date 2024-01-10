68cff0ed1b568f5f8a60a167d28c9969b0b9b14f
Dagim Fikru <95189710+Dagim-Fikru@users.noreply.github.com>
2024-01-10 18:59:58 +0300

    Add solution for Minimum Size Subarray Sum

---
+++ week-7/leetcode/minimum-size-subarray-sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        i=0
+        total = 0
+        res= len(nums)+1
+        for r in range(len(nums)):
+            total+=nums[r]
+            while total>=target:
+                res = min(r-i+1,res)
+                total-=nums[i]
+                i+=1
+        if res>=len(nums)+1:
+            return 0
+        return res
+
+            
