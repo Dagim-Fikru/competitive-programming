f8ebc06274bde701da59b40e3a35c492da4284b7
Dagim Fikru <95189710+Dagim-Fikru@users.noreply.github.com>
2024-01-15 21:04:24 +0300

    Add solution for Maximize the Confusion of an Exam

---
+++ week-7/leetcode/maximize-the-confusion-of-an-exam.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
+        F,T,i,j,ans = 0,0,0,0,0
+        
+        while j < len(answerKey):
+            if answerKey[j] == 'F':
+                F += 1
+            else:
+                T += 1
+            while min(F, T) > k:
+                if answerKey[i] == 'F':
+                    F -= 1
+                else:
+                    T -= 1
+                i += 1
+            ans = max(ans, F + T)
+            j += 1
+        return ans
+        
