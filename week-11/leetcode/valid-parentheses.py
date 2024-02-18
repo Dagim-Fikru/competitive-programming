1555689c567d37da8b621d57e1cc81f384c7ef4a
Dagim Fikru <95189710+Dagim-Fikru@users.noreply.github.com>
2024-02-18 21:28:21 +0300

    Add solution for Valid Parentheses

---
+++ week-11/leetcode/valid-parentheses.py
@@ -0,0 +1,47 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        isYes= True
+        stackOfChar = []
+        # stackOfChar[0]=j
+        for i in s:
+            # if len(stackOfChar)==0:
+            if i=='(' or i=='[' or i=='{':
+                stackOfChar.append(i)
+                # else:
+                #     break
+            else:
+                if len(stackOfChar)!=0:
+                    if i==')':
+                        if stackOfChar[-1]=='(':
+                            stackOfChar.pop()
+                        else:
+                            isYes=False
+                            break
+                    elif i==']':
+                        if stackOfChar[-1]=='[':
+                            stackOfChar.pop()
+                        else:
+                            isYes=False
+                            break
+                    elif i=='}':
+                        if stackOfChar[-1]=='{':
+                            stackOfChar.pop()
+                        else:
+                            isYes=False
+                            break
+                # elif len(stackOfChar)==1:
+                #     isYes=False
+                else:
+                    isYes = False
+                    break
+        if len(stackOfChar)!=0:
+            isYes=False
+                        
+            # if len(stackOfChar)==0 and (i==')' or i==']' or i=='}'):
+                # isYes = False
+                # break
+                
+        return isYes
+        
+        
+        
