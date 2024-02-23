1cb2f282793f3b8ad8338b6469188462335e866d
Dagim Fikru <95189710+Dagim-Fikru@users.noreply.github.com>
2024-02-23 15:12:14 +0300

    Add solution for Reduction Operations to Make the Array Elements Equal

---
+++ week-12/leetcode/reduction-operations-to-make-the-array-elements-equal.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reductionOperations(self, nums: List[int]) -> int:
+        frequency = Counter(nums)
+        minArr = list(set(nums))
+        if len(minArr)==1:
+            return 0
+        minArr.sort(reverse=True)
+        num_of_operation = 0
+        for i in range(len(minArr)-1):
+            num_of_operation+=frequency[minArr[i]]
+            frequency[minArr[i+1]]+=frequency[minArr[i]]
+        return num_of_operation
