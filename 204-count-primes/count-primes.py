0226231ca30ca859209378589aded2aa5389db02
Dagim Fikru <95189710+Dagim-Fikru@users.noreply.github.com>
2024-03-28 17:36:25 +0300

    Time: 2107 ms (32.90%) | Memory: 85.2 MB (30.41%) - LeetSync

---
+++ 204-count-primes/count-primes.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        #the Sieve of Eratosthenes algorithm
+        prime = [True for i in range(n+1)]
+        p = 2
+        while p**2 < n:
+            if prime[p]:
+                for i in range(p**2, n+1, p):
+                    prime[i] = False
+            p += 1
+        primes = [i for i in range(2, n) if prime[i]]
+        return len(primes)
+        
