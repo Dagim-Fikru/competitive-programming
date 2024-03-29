b066d3c9a0fe751cefb696335914ae65521571e7
Dagim Fikru <95189710+Dagim-Fikru@users.noreply.github.com>
2024-03-29 23:09:47 +0300

    Time: 188 ms (5.30%) | Memory: 37.2 MB (9.94%) - LeetSync

---
+++ 950-x-of-a-kind-in-a-deck-of-cards/x-of-a-kind-in-a-deck-of-cards.py
@@ -0,0 +1,24 @@
+import numpy as np
+class Solution:
+    def hasGroupsSizeX(self, deck: List[int]) -> bool:
+        arr = []
+        Count = Counter(deck)
+        ans = math.gcd()
+        for key,value in Count.items():
+            arr.append(value)
+        check = np.array(arr)
+        result = np.gcd.reduce(check)
+        if result<=1:
+            return False
+        return True
+        
+        # if len(deck)<=1:
+        #     return False
+        # for key,indx in Count.items():
+        #     if indx<check:
+        #         if check%indx:
+        #             return False
+        #     else:
+        #         if indx%check:
+        #             return False
+        # return True
