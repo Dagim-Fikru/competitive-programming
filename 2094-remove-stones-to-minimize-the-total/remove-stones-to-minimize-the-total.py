856243cb92f946d18a0a696fd518ee3ec2972f06
Dagim Fikru <95189710+Dagim-Fikru@users.noreply.github.com>
2024-04-18 19:38:05 +0300

    Time: 1279 ms (59.09%) | Memory: 31.1 MB (47.47%) - LeetSync

--- 2094-remove-stones-to-minimize-the-total/remove-stones-to-minimize-the-total.py
+++ 2094-remove-stones-to-minimize-the-total/remove-stones-to-minimize-the-total.py
@@ -6,8 +6,4 @@ class Solution:
             val = -heapq.heappop(heap)
             heapq.heappush(heap,-(abs(val)-(abs(val)//2)))
             k-=1
-        ans = 0
-
-        for i in heap:
-            ans+=abs(i)
-        return ans
+        return -sum(heap)
