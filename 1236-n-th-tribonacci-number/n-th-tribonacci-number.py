a802862c4df2aca747744cff398cab20af66a424
Dagim Fikru <95189710+Dagim-Fikru@users.noreply.github.com>
2024-05-16 18:45:24 +0300

    Time: 39 ms (22.93%) | Memory: 16.6 MB (32.58%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -1,13 +1,23 @@
 class Solution:
-    def __init__(self):
-        self.memo = {}
+    # def __init__(self):
+    #     self.memo = {}
     def tribonacci(self, n: int) -> int:
-        if n==0:
-            return n
-        elif n==1 or n==2:
-            return 1
-        else:
-            if n not in self.memo:
-                self.memo[n]=self.tribonacci(n-3)+self.tribonacci(n-1) + self.tribonacci(n-2)
-            return self.memo[n]
+        dp = [0] * (n+1)
+        if n==1:
+            dp[1]=1
+        elif n>=2:
+            dp[1]=1
+            dp[2]=1
+        for i in range(3,n+1):
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+        return dp[n]
+
+        # if n==0:
+        #     return n
+        # elif n==1 or n==2:
+        #     return 1
+        # else:
+        #     if n not in self.memo:
+        #         self.memo[n]=self.tribonacci(n-3)+self.tribonacci(n-1) + self.tribonacci(n-2)
+        #     return self.memo[n]
             
