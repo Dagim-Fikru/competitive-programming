a949eeae0950f2bd364ca726e11c5b28d557d42f
Dagim Fikru <95189710+Dagim-Fikru@users.noreply.github.com>
2024-09-04 22:52:12 +0300

    Time: 43 ms (16.76%) | Memory: 16.7 MB (6.98%) - LeetSync

---
+++ 926-find-and-replace-pattern/find-and-replace-pattern.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findAndReplacePattern(self, words: List[str], pattern: str) -> List[str]:
+        ans = []
+        pat = Counter(pattern)
+        for word in words:
+            check = {}
+            found = True
+            for i in range(len(pattern)):
+                if pattern[i] in check:
+                    if check[pattern[i]]!=word[i]:
+                        found=False
+                        break
+                elif word[i] in check.values():
+                    if pattern[i] in check:
+                        if check[pattern[i]]!=word[i]:
+                            found=False
+                            break
+                    else:
+                        found=False
+                        break
+                else:
+                    check[pattern[i]]=word[i]
+            if found:
+                ans.append(word)
+        return ans
