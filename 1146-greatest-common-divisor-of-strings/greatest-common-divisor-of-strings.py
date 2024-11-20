a56cefad59e11d5e9608cf5eac92130e84922f40
Dagim Fikru <95189710+Dagim-Fikru@users.noreply.github.com>
2024-11-20 11:54:55 +0300

    Time: 0 ms (100.00%) | Memory: 16.6 MB (39.31%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -1,9 +1,10 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
         ans = ""
+        lenOfAns = math.gcd(len(str1),len(str2))
         if str1+str2==str2+str1:
             for char in str1+str2:
-                if len(ans)!=math.gcd(len(str1),len(str2)):
+                if len(ans)!=lenOfAns:
                     ans+=char
                 else:
                     break
